cc9e2c3694edbba6840f396e9cf3a8ee7ced9902
piperpeaches <itoasted319@gmail.com>
2026-05-13 10:17:27 -0400

    Banner Updates

--- src/routes/componants/Main/Banner.svelte
+++ src/routes/componants/Main/Banner.svelte
@@ -1,3 +1,11 @@
-<div class="rounded gap-2 w-full border-white border p-5">
-    <p>Parodon My Dust! I'm Starting From Scratch and doing a full site remodel!</p>
+<script>
+import errorImg from '$lib/assets/error.png';
+</script>
+<div class="rounded border-yellow-300 text-yellow-300 border p-5 flex-row w-2/3 gap-5 justify-center items-center flex">
+    <div>
+        <p>Parodon My Dust! I'm Starting From Scratch and doing a full site remodel! You may see some Inconsistencies as I work through the changes. aswell as periodic updates</p>
+    </div>
+    <div>
+        <img src={errorImg} alt="Cheesecake" class="w-20">
+    </div>
 </div>
