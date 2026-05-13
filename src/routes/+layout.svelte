291c7c5e5afc21ca7368406a72d89383c86f21b0
piperpeaches <itoasted319@gmail.com>
2026-05-13 13:25:18 -0400

    Added Oneko.Svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Balatro from './componants/Shaders/Balatro.svelte';
+	import Oneko from './componants/oneko.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,5 +10,6 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <Balatro />
+<Oneko />
 
 {@render children()}
