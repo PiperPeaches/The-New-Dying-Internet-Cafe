1215a48a39a3bc614b47fc508577e396fc3d7c4b
piperpeaches <itoasted319@gmail.com>
2026-05-13 21:35:15 -0400

    Fixed Button for Online Build (hopefully)

--- src/routes/componants/button.svelte
+++ src/routes/componants/button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import buttonGif from '$lib/assets/button.gif';
     let { copyText = "Add my button to your site, it helps a ton!" } = $props();
 
     function handleClick() {
@@ -8,7 +9,7 @@
 
 <div class='flex flex-col p-3 w-full h-35 justify-center sm-h-90 gap-5 rounded border-1 bg-green cursor-pointer' on:click={handleClick}>
     <div class='justify-center flex items-center'>
-        <img src='src/lib/assets/button.gif' class='rounded' width={88} height={31} alt='Button' />
+        <img src={buttonGif} class='rounded' width={88} height={31} alt='Button' />
     </div>
 
     <p class='text-center text-sm'>Add my button to your site, it helps a ton!</p>
